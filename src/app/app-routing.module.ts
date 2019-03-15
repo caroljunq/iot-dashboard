256c1836fe75625d9311263956d957f864446ffc
Luis Puhl <luispuhl@gmail.com>
2019-03-15 07:22:23 -0300

    Using non-hash url

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -25,7 +25,7 @@ const routes: Routes = [
 @NgModule({
   imports: [
     // hash is required by firebase
-    RouterModule.forRoot(routes, {useHash: true}),
+    RouterModule.forRoot(routes),
     DashboardModule,
     HistoricalDataModule,
   ],
